1dc27c7ec067437ef2b8dd5997c9327b5aa28627
Rodrigo Cesar Panini <rodrigo_panini@hotmail.com>
2024-02-08 19:59:38 -0300

    Controller Pessoa

---
+++ VSComMariaDB/Controllers/PessoaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VSComMariaDB.Model;
+
+namespace VSComMariaDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet]
+        public List<Pessoa> GetLista()
+        {
+            var _DbContext = new _DbContext();
+            var vLista = _DbContext.Pessoa.ToList();
+
+            return vLista;
+        }
+    }
+}

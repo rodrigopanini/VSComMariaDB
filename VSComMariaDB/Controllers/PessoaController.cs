3cc22a2f50d190dd98ccb8b99c058051f8ed12c6
Rodrigo Cesar Panini <rodrigo_panini@hotmail.com>
2024-02-09 20:53:03 -0300

    Implementar Async

--- VSComMariaDB/Controllers/PessoaController.cs
+++ VSComMariaDB/Controllers/PessoaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using VSComMariaDB.Model;
 
 namespace VSComMariaDB.Controllers
@@ -8,6 +9,10 @@ namespace VSComMariaDB.Controllers
     [ApiController]
     public class PessoaController : ControllerBase
     {
+        /// <summary>
+        /// Pegar a lista de todas as pessoas
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public List<Pessoa> GetLista()
         {
@@ -15,6 +20,74 @@ namespace VSComMariaDB.Controllers
             var vLista = _DbContext.Pessoa.ToList();
 
             return vLista;
+        }
+
+        [HttpGet("ListaAsync")]
+        public async Task<List<Pessoa>> GetListaAsync()
+        {
+            var _DbContext = new _DbContext();
+            var vLista = await _DbContext.Pessoa.ToListAsync();
+
+            return vLista;
+        }
+
+        /// <summary>
+        /// Pegar os dados de uma pessoa específica
+        /// </summary>
+        /// <param name="id">ID da Pessoa</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<Pessoa> GetPessoa(int id)
+        {
+            //Instanciar o banco de dados
+            var _DbContext = new _DbContext();
+
+            //Selecionar a Pessoa pelo id
+            /*var vPessoa = _DbContext.Pessoa
+                .Where(p => p.Id == id)
+                .FirstOrDefault();*/
+
+            var vPessoa = await _DbContext.Pessoa.FindAsync(id);
+
+            //retornar dos dados
+            return vPessoa;
+        }
+
+        [HttpPost]
+        public async Task<List<Pessoa>> Inserir(List<Pessoa> pessoa)
+        {
+            var _DbContext = new _DbContext();
+
+            await _DbContext.Pessoa.AddRangeAsync(pessoa);
+            await _DbContext.SaveChangesAsync();
+
+            return pessoa;
+        }
+
+        [HttpPut]
+        public async Task<Pessoa> Alterar(Pessoa pessoa)
+        {
+            var _DbContext = new _DbContext();
+
+            _DbContext.Pessoa.Entry(pessoa).State = EntityState.Modified;
+            await _DbContext.SaveChangesAsync();
+
+            return pessoa;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Remover(int id)
+        {
+            //Instanciar o banco de dados
+            var _DbContext = new _DbContext();
+            //Localizar o registro a ser removido pelo id
+            var vPessoa = await _DbContext.Pessoa.FindAsync(id);
+            //Remover o registro encontrado
+            _DbContext.Pessoa.Remove(vPessoa);
+            //Salvar alterações
+            await _DbContext.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
